294e6a124b66773f1301c83f915df07c2fead5b1
I-Devil-69 <125646522+I-Devil-69@users.noreply.github.com>
2025-01-01 06:42:51 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+API_ID = config("29419742", default=None, cast=int)
+API_HASH = config("e2ffa892d1c47a8ad81d3d49fdf4f7b1", default=None)
+BOT_TOKEN = config("7208177037:AAFVBPx_nGs3AgR7-kozox8gfdpYlr1_mMI", default=None)
+SESSION = config("BQE8lCYAl5c6yWn_J8wYG5EtxPOLd_7TrR7PHQn_07IC6oaB22WbctrgJrwqsh4BWG5oruJ1q_fz3j4S3tdw7Zn50T5UxOrXQiLsGcTOfwPAQyV_M5s0oeMPruwz7diP8Y3d86Vh3Uj1A_ddGA_XCAtAxjl2BD64z9KZ2X73UugpabgLXX74vEzOqI28bsbTUz9OC3TIdLLxMkmU2oZA7l-EEOqnThjQQWUrp-OxQZdbS6BB9eoJ9pKAGfIvhS5y8wBmyY4CsQrF1KyVL_i-OjP8WaKdyWPb2RlW4u2YazlZZxM7-dLkywRMjnbz0Is-uTMv400WfVlgmWOHzwDr_qVFhcILRgAAAAGRAfg3AA", default=None)
+FORCESUB = config("demonarmy", default=None)
+AUTH = config("6727792695", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
